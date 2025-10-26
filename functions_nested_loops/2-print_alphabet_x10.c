5ee554c6661e3194e15afa85514c09a00082cc32
Nyde2482 <Callejoesteban@gmail.com>
2025-10-26 20:57:24 +0000

    w

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Fonction pour imprimer l'alphabet en minuscules x10
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ *
+ * Return: void
  */
-
 void print_alphabet_x10(void)
 {
 	char alphabet;
 	int nb_repeat;
 
-	for (nb_repeat = 0 ; nb_repeat < 10 ; nb_repeat++)
+	for (nb_repeat = 0; nb_repeat < 10; nb_repeat++)
 	{
-		for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{
 			_putchar(alphabet);
 		}
