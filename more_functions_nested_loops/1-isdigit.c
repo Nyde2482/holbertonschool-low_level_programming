12fa55b1f3a687e61697ed4d12b9eeb3838ce825
Nyde2482 <Callejoesteban@gmail.com>
2025-10-29 09:58:20 +0000

    feat : make a script that cheks for a digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/** 
+ * _isdigit - verif number
+ * @c : caractere verif
+ * Return: 1 if c is a digit otherwise 0 
+ */
+int _isdigit(int c)
+{
+if (c >= 1 && c <= 9)
+return (1);
+else
+return (0);
+}
+
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_h
 #define MAIN_h
 int _isupper(int c);
+int _isdigit(int c);
 #endif
