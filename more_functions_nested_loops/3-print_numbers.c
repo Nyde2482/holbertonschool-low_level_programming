d13f18c9e3e2018440bae5812c1e08b422d32c5a
Nyde2482 <Callejoesteban@gmail.com>
2025-10-29 13:27:09 +0000

    feat : ajout de la fonction print_numbers pour afficher les chiffres de 0 à 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_numbers - number 0 to 9
+ * followed by a line break
+ */
+void print_numbers(void)
+{
+char c;
+for (c ='o'; c <= '9'; c++)
+_putchar(c); // print current number
+_putchar('\n'); // line break at the end
+}
