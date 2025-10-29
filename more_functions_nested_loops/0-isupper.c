65289e41d049d935f58af0fecda291e7a9d37f42
Nyde2482 <Callejoesteban@gmail.com>
2025-10-29 09:13:30 +0000

    sa

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,13 @@
 #include "main.h"
-/** isupper 
-@c: caractere verifier
-Return: 1 si maj sinon 0
+/** 
+ * isupper
+ * @c: caractere verifier
+ * Return: 1 si maj sinon 0
  */
 int _isupper(int c)
 {
-    if (c >= 65 && c <= 90) 
-        return (1);
-    else
-        return (0);
+if (c >= 65 && c <= 90) 
+return (1);
+else
+return (0);
 }
