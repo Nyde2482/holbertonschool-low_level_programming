f010ccc2c6836caaaa090d5bd32a837ffd67d846
Nyde2482 <Callejoesteban@gmail.com>
2025-10-30 08:17:32 +0000

    feat : ajout de la fonction print_most_numbers pour afficher les chiffres de 0 à 9 sans 2 et 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_number - print number 0-9 without 2 and 4
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i != '2' && i != '4')
+		_putchar(i + '0');
+	}
+
+	_putchar('\n');
+}
+
