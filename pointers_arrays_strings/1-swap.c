5418bccbd900fb067bed9ad8fb21376d44d4e6d3
Nyde2482 <Callejoesteban@gmail.com>
2025-10-31 13:25:25 +0000

    feat : ajout de la fonction swap_int pour échanger les valeurs de deux entiers

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the value of *a and *b
+ * @a: pointer of a
+ * @b: pointer of b
+ */
+
+void swap_int(int *a, int *b);
+{
+	int temp = 0;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_h
 #define MAIN_h
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 #endif
