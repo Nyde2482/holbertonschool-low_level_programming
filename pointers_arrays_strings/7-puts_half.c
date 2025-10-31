16059e820147384410e803202a53220958f60104
Nyde2482 <Callejoesteban@gmail.com>
2025-10-31 20:09:11 +0000

    feat : ajout de la fonction puts_half pour afficher la seconde moitié d'une chaîne

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half os a string
+ *
+ * @str: string to print
+ */
+
+void puts_half(char *str)
+{
+	int lenght = 0;
+	int start = 0;
+
+	while (str[lenght] != '\0')
+	{
+		lenght++;
+	}
+
+	if (lenght % 2 == 0)
+	{
+		start = (lenght / 2);
+	}
+	else
+	{
+		start = ((lenght + 1) / 2);
+	}
+	for (lenght = start ; str[lenght] != '\0' ; lenght++)
+	{
+		_putchar(str[lenght]);
+	}
+
+	_putchar('\n');
+}
