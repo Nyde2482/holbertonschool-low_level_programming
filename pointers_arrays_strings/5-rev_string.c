219f499b9bf4591b7a4b1b3059249cb101be96a8
Nyde2482 <Callejoesteban@gmail.com>
2025-10-31 19:37:23 +0000

    feat : ajout de la fonction rev_string pour inverser une chaîne de caractères

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ *
+ * @s: the string to reverse
+ */
+
+void rev_string(char *s)
+{
+	int length = 0;
+	int a = 0;
+	int b = 0;
+	char temp = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (a = 0, b = length - 1 ; a < (length / 2) ; a++, b--)
+	{
+		temp = s[a];
+		s[a] = s[b];
+		s[b] = temp;
+	}
+}
