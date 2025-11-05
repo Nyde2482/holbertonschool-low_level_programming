8e865f0e1332d18da8c63a6f7333364d551cae46
Nyde2482 <Callejoesteban@gmail.com>
2025-11-05 17:58:54 +0000

    feat : ajout de la fonction _memset pour remplir la mémoire avec un octet constant

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -14,4 +14,5 @@ char *_strcpy(char *dest, char *src);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+char *_memset(char *s, char b, unsigned int n);
 #endif
