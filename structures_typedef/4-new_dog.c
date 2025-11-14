1da8254e6a7cb95b57a8640094479d2f2e437964
Nyde2482 <Callejoesteban@gmail.com>
2025-11-14 12:39:06 +0000

    fix : ajout de la documentation pour la fonction new_dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -5,9 +5,11 @@
 #include "dog.h"
 
 /**
+ * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ * Return: pointer to the new dog (Success), NULL (Fail)
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
