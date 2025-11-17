ed0c94553eb8496ebba8e2275b18e53f68ab9d2d
Nyde2482 <Callejoesteban@gmail.com>
2025-11-17 15:40:56 +0000

    feat : ajout de la fonction print_name pour afficher un nom via un pointeur de fonction

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name.
+ *
+ * @name: the string with the name to print
+ * @f: function pointer that takes a string argument
+ */
+
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+	{
+		return;
+	}
+	f(name);
+}
