1711e51e839bbb0e7d55ffb046f06f87a2aa7809
Nyde2482 <Callejoesteban@gmail.com>
2025-12-04 22:24:19 +0000

    style: amélioration de la mise en forme et de l'indentation dans les fonctions _strlen et add_node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,20 +1,23 @@
 #include "lists.h"
+
 /**
  * _strlen - compute length of a string (manual version)
  * @s: string
+ *
  * Return: length of string
  */
 int _strlen(const char *s)
 {
-int i = 0;
-while (s[i] != '\0')
-    i++;
-return (i);
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
 }
 
 /**
  * add_node - add a new node at the beginning of a list
- *
  * @head: pointer to the pointer of the first node
  * @str: the string to be stored in the new node
  *
@@ -23,18 +26,22 @@ return (i);
  */
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new_node = NULL;
-new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
- return (NULL);
-new_node->str = strdup(str);
-if (new_node->str == NULL)
-{
-free(new_node);
- return (NULL);
-}
-new_node->len = _strlen(str);
- new_node->next = *head;
- *head = new_node;
- return (new_node);
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = _strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
