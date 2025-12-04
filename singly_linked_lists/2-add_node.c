b294c45d6aed83ba9f6449da50b89064c4377bd2
Nyde2482 <Callejoesteban@gmail.com>
2025-12-04 22:19:30 +0000

    fix: correction de la documentation et amélioration de la gestion de la mémoire dans add_node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,33 +1,47 @@
 #include "lists.h"
 
 /**
- * add_node - add a new node at the begining of a list
+ * _strlen - compute length of a string (manual version)
+ * @s: string
+ * Return: length of string
+ */
+int _strlen(const char *s)
+{
+    int i = 0;
+
+    while (s[i] != '\0')
+        i++;
+
+    return (i);
+}
+
+/**
+ * add_node - add a new node at the beginning of a list
  *
  * @head: pointer to the pointer of the first node
- * @str: the string to be store in the new node
+ * @str: the string to be stored in the new node
  *
- * Return: adress of the new node
+ * Return: address of the new node
  *         NULL if memory allocation fails
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = NULL;
+    list_t *new_node = NULL;
 
-	new_node = malloc(sizeof(list_t));
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL);
 
-	if (new_node == NULL)
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
 
-	{
-		free(new_node);
-		new_node = NULL;
-		return (NULL);
-	}
+    new_node->len = _strlen(str);
+    new_node->next = *head;
+    *head = new_node;
 
-	new_node->str = strdup(str);
-	new_node->len = strlen(str);
-
-	new_node->next = *head;
-	*head = new_node;
-
-	return (*head);
+    return (new_node);
 }
