0ce0e9e0275799c84bafff94354c98424e0ef764
Nyde2482 <Callejoesteban@gmail.com>
2025-12-05 18:24:08 +0000

    feat: ajout de nouvelles fonctionnalités pour la gestion des listes doublement chaînées

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add a new node at the begining of a list
+ *
+ * @head: pointer to the pointer of the first node
+ * @n: the integer to be store in the new node
+ *
+ * Return: adress of the new node
+ *         NULL if memory allocation fails
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = NULL;
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+
+	if (head == NULL)
+		head = &new_node;
+	else if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		if (*head != NULL)
+			(*head)->prev = new_node;
+
+		*head = new_node;
+	}
+	return (new_node);
+}
